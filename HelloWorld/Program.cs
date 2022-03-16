66342483a92e440aae4b0fa2ae7ee9e7986cc46f
VivekOwhal43 <vivekowhal.co@gmail.com>
2022-03-16 16:38:09 +0530

    First commit for dot-net project.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine("Enter A number: ");
+//var number = System.Environment.MachineName;
+//Console.WriteLine("Your Machine Name is " +number);
+using System;
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var name = System.Environment.MachineName;
+            Console.WriteLine(name);
+        }
+    }
+}
